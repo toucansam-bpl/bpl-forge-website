3ddbf3c12fd19899d6b80d1f84f210bd6218a25f
toucansam_bpl <gumbyclownshoes@gmail.com>
2018-05-30 09:22:53 -0500

    Initial integration of Material Dashboard

---
+++ setup.md
@@ -0,0 +1,14 @@
+# Setup Notes for the app
+
+## Initial Creation
+https://github.com/facebook/create-react-app
+
+https://medium.com/@eliotjunior/prettier-eslint-facebook-code-quality-the-auto-magical-react-styling-tutorial-19481acb10dd
+
+https://prettier.io/docs/en/options.html
+https://medium.com/@BoweiHan/instantly-improve-your-code-by-setting-up-eslint-in-your-react-npm-project-34face702cbe
+https://medium.com/@BoweiHan/instantly-improve-your-code-by-setting-up-eslint-in-your-react-npm-project-34face702cbe
+
+
+## UI
+Material Dashboard Reac
---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
+
+import { createBrowserHistory } from 'history'
+import { Router, Route, Switch } from 'react-router-dom'
+
+import 'assets/css/material-dashboard-react.css?v=1.2.0'
+
+import indexRoutes from 'routes/index.jsx'
+
+const hist = createBrowserHistory()
+
+ReactDOM.render(
+  <Router history={hist}>
+    <Switch>
+      {indexRoutes.map((prop, key) => {
+        return <Route path={prop.path} component={prop.component} key={key} />
+      })}
+    </Switch>
+  </Router>,
+  document.getElementById('root')
+)
+registerServiceWorker()
